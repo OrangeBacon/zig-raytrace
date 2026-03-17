6a7df03396504e21c7c925e87a196286d8942170
Ella <ella.moreton20@gmail.com>
2026-03-17 14:16:51 +0000

    First ppm file

--- src/main.zig
+++ src/main.zig
@@ -2,26 +2,41 @@ const std = @import("std");
 const zig_raytrace = @import("zig_raytrace");
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try zig_raytrace.bufferedPrint();
-}
+    var gpa: std.heap.DebugAllocator(.{}) = .init;
+    const allocator = gpa.allocator();
+    defer {
+        const leak = gpa.deinit();
+        if (leak == .leak) std.debug.print("Warn: Memory Leak", .{});
+    }
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+    var args = try std.process.argsWithAllocator(allocator);
 
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
+    // skip the process name
+    _ = args.skip();
+
+    // get a passed in file path
+    var output: ?[:0]const u8 = null;
+    if (args.next()) |arg| {
+        output = arg;
+    }
+
+    // get the output file writer
+    var output_file = if (output) |name|
+        try std.fs.cwd().createFileZ(name, .{})
+    else
+        std.fs.File.stdout();
+    var output_buffer: [4096]u8 = undefined;
+    var writer = output_file.writer(&output_buffer);
+    defer writer.end() catch {
+        std.debug.panic("Error: Unable to complete file write", .{});
     };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+
+    // get the log file writer
+    var log_buffer: [4096]u8 = undefined;
+    var log = std.fs.File.stderr().writer(&log_buffer);
+
+    try zig_raytrace.rayTrace(f32, .{
+        .file = &writer.interface,
+        .log = &log.interface,
+    });
 }
